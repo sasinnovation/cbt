ad0516b24c4be87301d5e57a5b3864dca5221072
ADEBAY SAMUEL <114363603+Samwebconcept@users.noreply.github.com>
2026-05-07 17:18:50 +0100

    LEVEL 3 FIXED: production-ready system (prisma fixed + CI/CD + docker + clean architecture)

--- next.config.js
+++ next.config.js
@@ -3,10 +3,8 @@
   swcMinify: true,
   compress: true,
   productionBrowserSourceMaps: false,
-  eslint: { ignoreDuringBuilds: true },
-  images: { formats: ['image/avif', 'image/webp'] },
-  experimental: {
-    serverActions: true
+  images: {
+    formats: ['image/avif', 'image/webp']
   }
 };
 
